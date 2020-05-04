6aac458a9bb722591e52a84a49c73f8239e34412
Alu-care <53017255+Alu-care@users.noreply.github.com>
2020-05-04 21:17:01 +0200

    ajout DD

--- rsl-jpa/src/main/resources/data.sql
+++ rsl-jpa/src/main/resources/data.sql
@@ -1,4 +1,6 @@
 INSERT INTO hero (ID,english_Name,french_Name,url) VALUES ('1','Arbiter','Arbitre','https://www.alucare.fr/wiki/guide-raid-shadow-legend/guide-sur-arbitre-equipement-et-maitrise/');
 INSERT INTO hero (ID,english_Name,french_Name,url) VALUES ('2','Bad el Kazar','Bad el Kazar','https://www.alucare.fr/wiki/guide-raid-shadow-legend/guide-sur-bad-el-kazar-equipement-et-maitrise/');
-INSERT INTO hero (ID,english_Name,french_Name,url) VALUES ('3','Dracomorphe ','Dracomorphe ','https://www.alucare.fr/wiki/guide-raid-shadow-legend/guide-sur-dracomorphe-equipement-et-maitrise/');
+INSERT INTO hero (ID,english_Name,french_Name,url) VALUES ('3','Dracomorphe','Dracomorphe','https://www.alucare.fr/wiki/guide-raid-shadow-legend/guide-sur-dracomorphe-equipement-et-maitrise/');
+INSERT INTO hero (ID,english_Name,french_Name,url) VALUES ('4','Drexthar Doublesang','Drexthar Bloodtwin','https://www.alucare.fr/wiki/guide-raid-shadow-legend/guide-sur-drexthar-doublesang-drexthar-bloodtwin-artefact-et-maitrise/');
+
 
